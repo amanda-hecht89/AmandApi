b99c008edcfe53aa1ed745b4f724fea720bda320
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-07-27 17:54:42 -0600

    spellcheck on test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -9,7 +9,7 @@ describe('amandas routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('/amandas should return a list of amandas', () => {
+  it('/amandas should return a list of amandas', async() => {
     const res = await request(app).get('amandas');
     const expected = amandas.map((amanda) => {
       return { id: amanda.id, name: amanda.name };
