878ab3d277251c71c19d46bab2301f667d597fba
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-07-28 15:45:53 -0600

    it works

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,20 +3,18 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-const { amandas } = require('../models/Amanda');
+// const { amandas } = require('../models/Amanda');
 
 describe('amandas routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
   it('/amandas should return a list of amandas', async() => {
-    const res = await request(app).get('amandas');
-    const expected = amandas.map((amanda) => {
-      return { id: amanda.id, lastName: amanda.lastName };
-    });
-    expect(res.body).toEqual(expected);
+    const res = await request(app).get('/Amandas');
+    expect(res.body.length).toEqual(8);
+     
   });
-  afterAll(() => {
-    pool.end();
-  });
+});
+afterAll(() => {
+  pool.end();
 });
